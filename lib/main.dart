8ca3b32c74b0bec6035326d00ced5b73eea13b24
Arthur Ragimov <arthur.ragimov@gmail.com>
2020-05-05 14:10:52 +0300

    Inital project version

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp2/ui/home.dart';
+
+void main() => runApp(new MaterialApp(
+  home: QuizApp(),
+));
---
+++ lib/ui/home.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp2/model/question.dart';
+
+class QuizApp extends StatefulWidget {
+  @override
+  _QuizAppState createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  int _currentQuestionIndex = 0;
+
+  List questionBank = [
+    Question.name("Do you smoke?", true),
+    Question.name("Do you like a bicycle?", true),
+    Question.name("Do you ride a machine?", false),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("True Citizen"),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey,
+      ),
+      backgroundColor: Colors.blueGrey,
+
+      // We use [Builder] here to use a [context] that is a descendant of [Scaffold]
+      // or else [Scaffold.of] will return null
+      body: Builder(
+        builder: (BuildContext context) => Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Image.asset(
+                  "images/flag.png",
+                  width: 250,
+                  height: 180,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Container(
+                  height: 120.0,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(14.4),
+                    border: Border.all(
+                      color: Colors.blueGrey.shade400,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        questionBank[_currentQuestionIndex].questionText,
+                        style: TextStyle(
+                          fontSize: 16.9,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  RaisedButton(
+                    onPressed: () => _previousQuestion(),
+                    color: Colors.blueGrey.shade900,
+                    child: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _checkAnswer(true, context),
+                    color: Colors.blueGrey.shade900,
+                    child: Text(
+                      "TRUE",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _checkAnswer(false, context),
+                    color: Colors.blueGrey.shade900,
+                    child: Text(
+                      "FALSE",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  RaisedButton(
+                    onPressed: () => _nextQuestion(),
+                    color: Colors.blueGrey.shade900,
+                    child: Icon(
+                      Icons.arrow_forward,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _checkAnswer(bool userChoice, BuildContext context) {
+    debugPrint('_checkAnswer');
+    if (userChoice == questionBank[_currentQuestionIndex].isCorrect) {
+      // correct
+      final snackbar = SnackBar(
+        backgroundColor: Colors.green,
+        duration: Duration(
+          milliseconds: 500,
+        ),
+        content: Text("Correct!"),
+      );
+      Scaffold.of(context).showSnackBar(snackbar);
+
+      _updateQuestion();
+    } else {
+      // incorrect
+      final snackbar = SnackBar(
+        backgroundColor: Colors.redAccent,
+        duration: Duration(
+          milliseconds: 500,
+        ),
+        content: Text("Incorrect!"),
+      );
+      Scaffold.of(context).showSnackBar(snackbar);
+
+      _updateQuestion();
+    }
+  }
+
+  _updateQuestion() {
+    setState(() {
+      _currentQuestionIndex = (_currentQuestionIndex + 1) % questionBank.length;
+    });
+  }
+
+  _nextQuestion() => _updateQuestion();
+
+  _previousQuestion() {
+    setState(() {
+      _currentQuestionIndex = (_currentQuestionIndex - 1) % questionBank.length;
+    });
+  }
+}
